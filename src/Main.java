104609be81e16de47ca2e30745316ec56bff7d74
Changjo Hwang <hchj2005@gmail.com>
2023-12-29 16:06:06 +0900

    11653번 소인수분해

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 package src;
+// 11653번 소인수분해
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int primeNumber = 2;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (N > 1){
+            if (N % primeNumber == 0) {
+                N = N / primeNumber;
+                System.out.println(primeNumber);
+            }
+            else{
+                primeNumber++;
+            }
         }
     }
 }
