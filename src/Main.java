f2c78819d092763b7ee30c3e2c6cf990d48c6c5e
Changjo Hwang <hchj2005@gmail.com>
2024-01-02 17:05:27 +0900

    1269번 대칭 차집합

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,38 @@
 package src;
-// 11653번 소인수분해
+// 1269번 대칭 차집합
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int primeNumber = 2;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int cntA = Integer.parseInt(st.nextToken());
+        int cntB = Integer.parseInt(st.nextToken());
 
-        while (N > 1){
-            if (N % primeNumber == 0) {
-                N = N / primeNumber;
-                System.out.println(primeNumber);
-            }
-            else{
-                primeNumber++;
-            }
+        Set<Integer> setA = new HashSet<>();
+        Set<Integer> setB = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < cntA; i++) {
+            setA.add(Integer.parseInt(st.nextToken()));
         }
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < cntB; i++) {
+            setB.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int result = count(setA, setB) + count(setB, setA);
+        System.out.print(result);
+    }
+
+    public static int count(Set<Integer> a, Set<Integer> b){
+        Set<Integer> temp = new HashSet<>(a);
+        temp.removeAll(b);
+        return temp.size();
     }
 }
