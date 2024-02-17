d8a7a62f2e5f40ba5c806eca5928d8976cf7b989
Changjo Hwang <hchj2005@gmail.com>
2024-02-17 17:52:47 +0900

    # 1644번 소수의 연속합

--- src/Main.py
+++ src/Main.py
@@ -1,40 +1,51 @@
-# 2568번 전깃줄 2
-from bisect import bisect_left
+# 1644번 소수의 연속합
 import sys
 input = sys.stdin.readline
 
+
+def prime_list(number):
+    # 자기 자신도 포함
+    n = number+1
+
+    # 에라토스테네스의 체 초기화: n개 요소에 True 설정(소수로 간주)
+    sieve = [True] * n
+
+    # n의 최대 약수가 sqrt(n) 이하이므로 i=sqrt(n)까지 검사
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i]:  # i가 소수인 경우
+            for j in range(i + i, n, i):  # i이후 i의 배수들을 False 판정
+                sieve[j] = False
+
+    # 소수 목록 산출
+    return [i for i in range(2, n) if sieve[i] == True]
+
+
 N = int(input())
-line_list = []
-for _ in range(N):
-    line_list.append(list(map(int, input().split())))
-line_list.sort(key=lambda x: x[0])
+cnt = 0
+start = 0
+end = 0
+primes = prime_list(N)
+if N != 1:
+    current = primes[0]
+else:
+    current = -1
 
-select_line_list = []
-check = []
-for line in line_list:
-    if not select_line_list:
-        select_line_list.append(line[1])
-    if line[1] > select_line_list[-1]:
-        select_line_list.append(line[1])
-        check.append((len(select_line_list)-1, line[1]))
+while start <= end:
+    if current == -1:
+        break
+
+    if current == N:
+        cnt += 1
+        current -= primes[start]
+        start += 1
+    elif current < N:
+        if end == len(primes)-1:
+            break
+        end += 1
+        current += primes[end]
     else:
-        index = bisect_left(select_line_list, line[1])
-        select_line_list[index] = line[1]
-        check.append((index, line[1]))
+        current -= primes[start]
+        start += 1
 
-backtrace = []
-last_index = len(select_line_list)-1
-for i in range(len(check)-1, -1, -1):
-    if check[i][0] == last_index:
-        backtrace.append(check[i][1])
-        last_index -= 1
-
-print(N - len(backtrace))
-
-delete_line_list = []
-for line in line_list:
-    if line[1] not in backtrace:
-        delete_line_list.append(line[0])
-delete_line_list.sort()
-
-print(*delete_line_list, sep='\n')
+print(cnt)
