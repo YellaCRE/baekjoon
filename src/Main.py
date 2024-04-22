e47230a9f8e2c4aa271ac3c746a03e3735eac7e2
Changjo Hwang <hchj2005@gmail.com>
2024-04-23 01:08:05 +0900

    1049번 기타줄

--- src/Main.py
+++ src/Main.py
@@ -1,17 +1,24 @@
-# 1026번 보물
+# 1049번 기타줄
 import sys
 input = sys.stdin.readline
 
+N, M = map(int, input().split())
+bundle_string = int(1e9)
+single_string = int(1e9)
+for _ in range(M):
+    bundle_price, single_price = map(int, input().split())
+    bundle_string, single_string = min(bundle_string, bundle_price), min(single_string, single_price)
+
 answer = 0
-N = int(input())
-A_list = list(map(int, input().split()))
-B_list = list(map(int, input().split()))
-
-A_list.sort()
-
-for idx in range(N):
-    B_max = max(B_list)
-    answer += B_max * A_list[idx]
-    B_list.remove(B_max)
+if single_string * 6 <= bundle_string:
+    answer = single_string * N
+else:
+    bundle_cnt = N // 6
+    mod = N % 6
+    answer += bundle_cnt * bundle_string
+    if mod * single_string > bundle_string:
+        answer += bundle_string
+    else:
+        answer += mod * single_string
 
 print(answer)
