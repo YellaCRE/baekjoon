a2f49733a48fb15a8210b036f4c7f4910fd5bf61
Changjo Hwang <hchj2005@gmail.com>
2024-04-24 02:34:18 +0900

    11000번 강의실 배정

--- src/Main.py
+++ src/Main.py
@@ -1,24 +1,25 @@
-# 1049번 기타줄
+# 11000번 강의실 배정
+from heapq import heappush, heappop
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-bundle_string = int(1e9)
-single_string = int(1e9)
-for _ in range(M):
-    bundle_price, single_price = map(int, input().split())
-    bundle_string, single_string = min(bundle_string, bundle_price), min(single_string, single_price)
+N = int(input())
+q = []
 
-answer = 0
-if single_string * 6 <= bundle_string:
-    answer = single_string * N
-else:
-    bundle_cnt = N // 6
-    mod = N % 6
-    answer += bundle_cnt * bundle_string
-    if mod * single_string > bundle_string:
-        answer += bundle_string
-    else:
-        answer += mod * single_string
+for i in range(N):
+    start, end = map(int, input().split())
+    q.append([start, end])
 
-print(answer)
+q.sort()
+
+room = []
+heappush(room, q[0][1])
+
+for i in range(1, N):
+    if q[i][0] < room[0]: # 현재 회의실 끝나는 시간보다 다음 회의 시작시간이 빠르면
+        heappush(room, q[i][1]) # 새로운 회의실 개설
+    else: # 현재 회의실에 이어서 회의 개최 가능
+        heappop(room) # 새로운 회의로 시간 변경을 위해 pop후 새 시간 push
+        heappush(room, q[i][1])
+
+print(len(room))
