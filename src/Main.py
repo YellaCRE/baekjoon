a0e82cc1049754ff201a19e1035d57b813cc453c
Changjo Hwang <hchj2005@gmail.com>
2024-04-26 00:52:58 +0900

    9406번 텀 프로젝트

--- src/Main.py
+++ src/Main.py
@@ -1,50 +1,35 @@
-# 1103번 게임(개선)
+# 9406번 텀 프로젝트
 import sys
+sys.setrecursionlimit(int(1e6))
 input = sys.stdin.readline
-sys.setrecursionlimit(int(1e5))
-dr = [1, 0, -1, 0]
-dc = [0, 1, 0, -1]
 
 
-def is_in_range(r, c):
-    return 0 <= r < N and 0 <= c < M
-
-
-def coin_move(r, c, cnt):
+def find_team(i):
     global result
-    result = max(result, cnt)
+    visited[i] = True
+    cycle.append(i)
 
-    move_dist = int(board[r][c])
-    for d in range(4):
-        nxt_row, nxt_col = r + move_dist * dr[d], c + move_dist * dc[d]
-
-        if not is_in_range(nxt_row, nxt_col):
-            continue
-
-        if board[nxt_row][nxt_col] == 'H':
-            continue
-
-        if dp[r][c]+1 <= dp[nxt_row][nxt_col]:
-            continue
-
-        if visited[nxt_row][nxt_col]:
-            print(-1)
-            exit()
-
-        dp[nxt_row][nxt_col] = cnt + 1
-        visited[nxt_row][nxt_col] = True
-        coin_move(nxt_row, nxt_col, cnt+1)
-        visited[nxt_row][nxt_col] = False
+    nxt_member = team_select[i]
+    if visited[nxt_member]:
+        if nxt_member in cycle:
+            start_idx = cycle.index(nxt_member)
+            result += cycle[start_idx:]
+        return
+    else:
+        find_team(nxt_member)
 
 
-result = 0
-N, M = map(int, input().split())
-dp = [[0 for _ in range(M)] for _ in range(N)]
-board = []
-for _ in range(N):
-    board.append(list(input()))
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    team_select = list(map(int, input().split()))
+    team_select.insert(0, 0)
+    visited = [False for _ in range(N+1)]
+    result = []
 
-visited = [[False for _ in range(M)] for _ in range(N)]
-coin_move(0, 0, 0)
+    for idx in range(1, N+1):
+        if not visited[idx]:
+            cycle = []
+            find_team(idx)
 
-print(result + 1)
+    print(N - len(result))
