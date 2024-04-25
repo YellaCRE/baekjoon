0284cf27127718b7dc60cd5cc3a50a53d4b197b4
Changjo Hwang <hchj2005@gmail.com>
2024-04-26 00:58:56 +0900

    9406번 텀 프로젝트(주석 추가)

--- src/Main.py
+++ src/Main.py
@@ -11,8 +11,11 @@ def find_team(i):
 
     nxt_member = team_select[i]
     if visited[nxt_member]:
+        # 만약 visited인데 사이클에 없으면 그 멤버는 어느 프로젝트 팀에도 속할 수 없다
+        # 이미 visited라는 뜻은 선택권을 사용했다는 의미, 만약 선택권을 사용했는데 선택받지 못했다면 다른 경우의 수는 나올 수 없다
         if nxt_member in cycle:
             start_idx = cycle.index(nxt_member)
+            # 리스트를 더하기 연산하면 펼쳐서 값을 append 할 수 있다
             result += cycle[start_idx:]
         return
     else:
