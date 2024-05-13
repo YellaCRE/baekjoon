26d31ceba325976d24fdfae526e94bcff7429a83
Changjo Hwang <hchj2005@gmail.com>
2024-05-13 15:46:07 +0900

    1912번 연속합

--- src/Main.py
+++ src/Main.py
@@ -1,32 +1,15 @@
-# 29791번 에르다 노바와 오리진 스킬
 import sys
-input = sys.stdin.readline
-IMMUNE = 90
+INPUT = sys.stdin.readline
 
+# 1912번 연속합
+N = int(INPUT())
+num_list = list(map(int, INPUT().split()))
+dp = [0 for _ in range(N)]
+dp[0] = num_list[0]
+for i in range(1, N):
+    # 이전까지의 max 값이 dp에 저장되기 때문에 
+    # 다음 값만 고려해서 max 함수를 사용하면 된다
+    dp[i] = max(num_list[i], dp[i-1]+num_list[i])
 
-def check_skill_cnt(skill_set, cool):
-    skill_cnt = 0
-    immune_time = 0
-    cool_time = 0
-    for timing in skill_set:
-        if timing >= immune_time and timing >= cool_time:
-            immune_time = timing + IMMUNE
-            cool_time = timing + cool
-            skill_cnt += 1
-
-    return skill_cnt
-
-
-N, M = map(int, input().split())
-elda_skill_timing = set(map(int, input().split()))
-origin_skill_timing = set(map(int, input().split()))
-
-# 기록들이 바람에 날려 뒤죽박죽 섞이게 되었다
-elda_sorted_set = sorted(elda_skill_timing)
-origin_sorted_set = sorted(origin_skill_timing)
-
-# 에르다 = 행동 불가, 오리진 = 절대 행동 불가
-# 에르다 노바의 재사용 대기 시간은 100초, 오리진 스킬의 재사용 대기 시간은 360초다
-# 맞으면 각각의 스킬에 대해 90초 동안 면역
-# 몬스터가 행동 불가와 절대 행동 불가 상태가 된 횟수를 구하라
-print(check_skill_cnt(elda_sorted_set, 100), check_skill_cnt(origin_sorted_set, 360))
+# 그리고 max(dp)를 통해 이전 max 값들 중 가장 큰 값을 찾으면 된다
+print(max(dp))
