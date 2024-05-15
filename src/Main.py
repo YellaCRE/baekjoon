40b00e6ec4acd418d0555dcd12241ad6b7d4a9b0
Changjo Hwang <hchj2005@gmail.com>
2024-05-16 01:18:17 +0900

    29792번 규칙적인 보스돌이

--- src/Main.py
+++ src/Main.py
@@ -1,15 +1,44 @@
 import sys
 INPUT = sys.stdin.readline
+MAX_TIME = 15 * 60
 
-# 1912번 연속합
-N = int(INPUT())
-num_list = list(map(int, INPUT().split()))
-dp = [0 for _ in range(N)]
-dp[0] = num_list[0]
-for i in range(1, N):
-    # 이전까지의 max 값이 dp에 저장되기 때문에 
-    # 다음 값만 고려해서 max 함수를 사용하면 된다
-    dp[i] = max(num_list[i], dp[i-1]+num_list[i])
+# 29792번 규칙적인 보스돌이
+N, M, K = map(int, INPUT().split())
 
-# 그리고 max(dp)를 통해 이전 max 값들 중 가장 큰 값을 찾으면 된다
-print(max(dp))
+damage_list = []
+for _ in range(N):
+    damage_list.append(int(input()))
+
+# 공격력 순 정렬, 강한 M개의 캐릭터 사용
+damage_list.sort(reverse=True)
+
+# [보스의 체력, 드랍하는 메소]
+boss_list = []
+for _ in range(K):
+    boss_list.append(list(map(int, input().split())))
+
+
+def maxMeso(d):
+    dp = [0] * (MAX_TIME + 1)
+    for hp, value in boss_list:
+        # 보스를 잡기 위해 걸리는 시간
+        time = (hp + d - 1) // d
+        # 만약 15분 이내에 잡을 수 없다면 continue
+        if MAX_TIME < time:
+            continue
+
+        for j in range(MAX_TIME, 0, -1):
+            if j - time >= 0 and dp[j - time] != 0:
+                dp[j] = max(dp[j], dp[j - time] + value)
+
+        dp[time] = max(dp[time], value)
+
+    return max(dp)
+
+
+total_coin = 0
+# M개의 캐릭터 사용
+for i in range(M):
+    total_coin += maxMeso(damage_list[i])
+
+print(total_coin)
