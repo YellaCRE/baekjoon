9efb1a0fdccf9e946a00791f0455a69a62abd1cf
Changjo Hwang <hchj2005@gmail.com>
2024-05-18 23:46:59 +0900

    2533번 사회망 서비스

--- src/Main.py
+++ src/Main.py
@@ -1,40 +1,35 @@
 import sys
-from itertools import combinations
-from bisect import bisect_left, bisect_right
+sys.setrecursionlimit(int(1e9))
 INPUT = sys.stdin.readline
 
+# 2533번 사회망 서비스
+N = int(INPUT())
+graph = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    start, end = map(int, INPUT().split())
+    graph[start].append(end)
+    graph[end].append(start)
 
-def getSubSequence(arr, sumArr):
-    for cnt in range(1, len(arr)+1):
-        for combiSum in combinations(arr, cnt):
-            sumArr.append(sum(combiSum))
+dp = [[0, 1] for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+visited[1] = True
 
 
-def getNumCnt(arr, find):
-    # 찾는 숫자가 여러 개일 수 있다
-    return bisect_right(arr, find) - bisect_left(arr, find)
+def dfs(parent_node):
+    for child_node in graph[parent_node]:
+        # 아직 방문하지 않은 노드일 경우
+        if not visited[child_node]:
+            visited[child_node] = True
+            # 일단 리프 노드까지 도착한다
+            dfs(child_node)
+            # 자식 노드가 없으면 return 되면서 실행되기 때문에
+            # 리프 노드부터 컨텍스트 실행
+
+            # 부모 노드가 얼리어답터가 아닌 경우, 자식 노드는 반드시 얼리어답터이다
+            dp[parent_node][0] += dp[child_node][1]
+            # 부모 노드가 얼리어답터인 경우, 자식노드는 상관없음(작은 수)
+            dp[parent_node][1] += min(dp[child_node])
 
 
-# 1208번 부분수열의 합 2
-N, S = map(int, INPUT().split())
-num_list = list(map(int, INPUT().split()))
-
-left, right = num_list[:N//2], num_list[N//2:]
-left_sum, right_sum = [], []
-
-getSubSequence(left, left_sum)
-getSubSequence(right, right_sum)
-# 정렬을 하는 이유는 bisect를 통해 개수를 구할 것이라
-left_sum.sort()
-right_sum.sort()
-
-ans = 0
-for le in left_sum:
-    find_num = S - le
-    # find_num 의 개수 찾기
-    ans += getNumCnt(right_sum, find_num)
-
-ans += getNumCnt(left_sum, S)
-ans += getNumCnt(right_sum, S)
-
-print(ans)
+dfs(1)
+print(min(dp[1]))
