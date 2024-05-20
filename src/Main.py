d7d7f10f01d8d225aafea531933b4db20747f942
Changjo Hwang <hchj2005@gmail.com>
2024-05-20 22:27:26 +0900

    2753번 윤년

--- src/Main.py
+++ src/Main.py
@@ -1,27 +1,10 @@
 import sys
 INPUT = sys.stdin.readline
 
-# 인하대 A번 창고지기
-graph = list(INPUT().strip())
+# 2753번 윤년
+year = int(INPUT())
 
-robot_idx, box_idx, goal_idx = -1, -1, -1
-for idx in range(len(graph)):
-    if graph[idx] == '@':
-        robot_idx = idx
-    if graph[idx] == '#':
-        box_idx = idx
-    if graph[idx] == '!':
-        goal_idx = idx
-
-
-def solve(robot, box, goal):
-    if not (robot < box < goal or goal < box < robot):
-        return -1
-
-    robot_move = abs(box - robot) - 1
-    box_move = abs(goal - box)
-
-    return robot_move + box_move
-
-
-print(solve(robot_idx, box_idx, goal_idx))
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print(1)
+else:
+    print(0)
