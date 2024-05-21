c94389b1de6a57a372210e10d46fb0f8f1e4548f
Changjo Hwang <hchj2005@gmail.com>
2024-05-22 00:14:12 +0900

    1051번 숫자 정사각형

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,43 @@
 package src;
-// 10815번 숫자 카드
+// 1051번 숫자 정사각형
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.*;
 
 public class Main {
+    static int N, M;
+    static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer stN = new StringTokenizer(br.readLine());
-        ArrayList<Integer> nCards = new ArrayList<>();
-        for (int i = 0; i<N; i++){
-            nCards.add(Integer.parseInt(stN.nextToken()));
-        }
-        Collections.sort(nCards);
-
-        int M = Integer.parseInt(br.readLine());
-        StringTokenizer stM = new StringTokenizer(br.readLine());
-        ArrayList<Integer> mCards = new ArrayList<>();
-        for (int i = 0; i<M; i++){
-            mCards.add(Integer.parseInt(stM.nextToken()));
-        }
-
-        for (int card : mCards){
-            sb.append(bisect(card, N-1, nCards));
-            sb.append(" ");
-        }
-        System.out.print(sb);
-    }
-
-    public static int bisect(int card, int N, ArrayList<Integer> cards){
-        int left = 0;
-        int right = N;
-        int mid;
-
-        while (left <= right){
-            mid = (left + right) / 2;
-            if (card == cards.get(mid)){
-                return 1;
-            }
-
-            if (card < cards.get(mid)){
-                right = mid-1;
-            } else {
-                left = mid+1;
+        for (int i = 0; i < N; i++){
+            String str = br.readLine();
+            for (int j=0; j < M; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
         }
-        return 0;
+
+        int len = Math.min(N, M);
+
+        while (len > 1) {
+            for (int i = 0; i<=N-len; i++) {
+                for (int j = 0; j<=M-len; j++) {
+                    int num = map[i][j];
+
+                    if (num == map[i][j+len-1] && num == map[i+len-1][j] && num == map[i+len-1][j+len-1]) {
+                        System.out.println(len * len);
+                        return;
+                    }
+                }
+            }
+            len--;
+        }
+
+        System.out.println(len * len);
     }
 }
